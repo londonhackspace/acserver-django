0d225d8ff2b6059a41f37c8f024639af12333a5c
Jasper Wallace <jasper@pointless.net>
2015-11-02 15:43:02 +0000

    a script to update from the carddb ans sync with ldap

---
+++ update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+error=0
+log=`mktemp`
+touch $log
+tmp=/var/www/acserver-django/tmp
+
+function check_ret ()
+{
+	te=$?
+	if [ $te -ne 0 ] ; then
+		error=$te
+		if [ ! -e ${tmp}/acserver-update-broken ] ; then
+			cat $log
+		fi
+		return 1
+	fi
+	return 0
+}
+
+wget https://london.hackspace.org.uk/carddb.php -O ${tmp}/carddb.json > $log 2>&1
+check_ret
+/var/www/acserver-django/manage.py updatecarddb ${tmp}/carddb.json > $log 2>&1
+check_ret
+rm -f ${tmp}/carddb.json
+/var/www/acserver-django/manage.py syncldap > $log 2>&1
+check_ret
+
+rm -f $log
+
+if [ $error -eq 0 ] ; then
+	if [ -e ${tmp}/acserver-update-broken ] ; then
+		echo "acserver update ok now"
+		rm -f ${tmp}/acserver-update-broken
+	fi
+else
+	touch ${tmp}/acserver-update-broken
+fi
