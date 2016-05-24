967d171b3768ef9b8cdcb5c2b475ce33d7315787
lol <samface@localhost.localdomain>
2016-05-24 22:32:26 +0100

    roll settings back from postgres to sqlite gubbins (pg stuff set elsewhere)

--- acserver/settings.py
+++ acserver/settings.py
@@ -59,18 +59,10 @@ WSGI_APPLICATION = 'acserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangoacserver',
-        'USER': 'djangouser',
-        'PASSWORD': 'djangololz',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
