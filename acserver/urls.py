e26df9a54aec343dfcb4b18dfaf8ce4dc02aa11f
Jasper Wallace <jasper@pointless.net>
2018-09-16 17:21:01 +0100

    Fix another depreciation: String views are now callables, #9

--- acserver/urls.py
+++ acserver/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+import server.views
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,29 +10,29 @@ urlpatterns = patterns('',
 
     url(r'^$', TemplateView.as_view(template_name="root")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^(?P<tool_id>\d+)/status/$', 'server.views.status'),
+    url(r'^(?P<tool_id>\d+)/status/$', server.views.status),
 #/1/card/12345678
-    url(r'^(?P<tool_id>\d+)/card/(?P<card_id>[a-fA-F0-9]+)$', 'server.views.card'),
+    url(r'^(?P<tool_id>\d+)/card/(?P<card_id>[a-fA-F0-9]+)$', server.views.card),
 #/1/grant-to-card/12345678/by-card/00112233445566
-    url(r'^(?P<tool_id>\d+)/grant-to-card/(?P<to_cardid>[a-fA-F0-9]+)/by-card/(?P<by_cardid>[a-fA-F0-9]+)$', 'server.views.granttocard'),
+    url(r'^(?P<tool_id>\d+)/grant-to-card/(?P<to_cardid>[a-fA-F0-9]+)/by-card/(?P<by_cardid>[a-fA-F0-9]+)$', server.views.granttocard),
 #/1/status/0/by/33333333
-    url(r'^(?P<tool_id>\d+)/status/(?P<status>[01])/by/(?P<card_id>[a-fA-F0-9]+)$', 'server.views.settoolstatus'),
+    url(r'^(?P<tool_id>\d+)/status/(?P<status>[01])/by/(?P<card_id>[a-fA-F0-9]+)$', server.views.settoolstatus),
 #/1/tooluse/1/22222222
-    url(r'^(?P<tool_id>\d+)/tooluse/(?P<status>[01])/(?P<card_id>[a-fA-F0-9]+)$', 'server.views.settooluse'),
+    url(r'^(?P<tool_id>\d+)/tooluse/(?P<status>[01])/(?P<card_id>[a-fA-F0-9]+)$', server.views.settooluse),
 #/1/is_tool_in_use
-    url(r'^(?P<tool_id>\d+)/is_tool_in_use$', 'server.views.isinuse'),
+    url(r'^(?P<tool_id>\d+)/is_tool_in_use$', server.views.isinuse),
 #/1/tooluse/time/for/22222222/5
-    url(r'^(?P<tool_id>\d+)/tooluse/time/for/(?P<card_id>[a-fA-F0-9]+)/(?P<duration>[0-9]+)$', 'server.views.settoolusetime'),
+    url(r'^(?P<tool_id>\d+)/tooluse/time/for/(?P<card_id>[a-fA-F0-9]+)/(?P<duration>[0-9]+)$', server.views.settoolusetime),
 #/api/get_tools_summary_for_user/1
-    url(r'^api/get_tools_summary_for_user/(?P<user_id>[0-9]+)$', 'server.views.get_tools_summary_for_user'),
+    url(r'^api/get_tools_summary_for_user/(?P<user_id>[0-9]+)$', server.views.get_tools_summary_for_user),
 #/api/get_tools_status
-    url(r'^api/get_tools_status$', 'server.views.get_tools_status'),
+    url(r'^api/get_tools_status$', server.views.get_tools_status),
 #/api/get_tool_runtime
-    url(r'^api/get_tool_runtime_since/(?P<tool_id>[0-9]+)/(?P<start_time>[0-9]+)$', 'server.views.get_tool_runtime'),
+    url(r'^api/get_tool_runtime_since/(?P<tool_id>[0-9]+)/(?P<start_time>[0-9]+)$', server.views.get_tool_runtime),
 #/api/get_user_name/12345678
-    url(r'^api/get_user_name/(?P<card_id>[a-fA-F0-9]+)$', 'server.views.get_user_name'),
+    url(r'^api/get_user_name/(?P<card_id>[a-fA-F0-9]+)$', server.views.get_user_name),
 #/uitest_calheatmap1
-    url(r'^uitest_calheatmap1$', 'server.views.calheatmap1'),  
+    url(r'^uitest_calheatmap1$', server.views.calheatmap1),  
 #/ac_card_usage
-    url(r'^ac_card_usage$', 'server.views.ac_card_usage', name='ac_card_usage')
+    url(r'^ac_card_usage$', server.views.ac_card_usage, name='ac_card_usage')
 )
